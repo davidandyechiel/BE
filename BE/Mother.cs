b7dad6b1ba269720da9be339c3a9375d4741718d
David Salem <dsalem9191@gmail.com>
2017-12-19 14:23:06 +0200

    added fields to mother

--- BE/Mother.cs
+++ BE/Mother.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace BE
 {
     class Mother
     {
+        private int id;
+        private string LastName;
+        private string FirstName;
+        private int homePhoneNum;
+        private int cellPhoneNum;
+        private string address;
+        private string addressNearHere;//
+        private int hoursNeeded;//how many hours per week needed
+        private int daysNeeded;//how many days per week needed 
+        private bool[] needNanny;//days of the week mother needs a nanny
+        private string notes;
+        private DateTime[,] hoursTable;//table that stores the start and end time of each day of the week 
+
+
+        public override string ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+
     }
 }
