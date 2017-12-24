eef8b2d30799291282c804638679d6b2e4d2928e
David Salem <dsalem9191@gmail.com>
2017-12-24 12:44:33 +0200

    12:45

--- BL/BL_Basic.cs
+++ BL/BL_Basic.cs
@@ -52,10 +52,10 @@ namespace BL
                 {
                     if (((Contract)obj).Ishourly)
                         ((Contract)obj).WagesPerHour = ((MyDal.getNannyDS().Find(x => x.Id == (((Contract)obj).NannysID)).HourRate) 
-                            * (findMotherFromContract((Contract)obj).HoursNeeded) * 4)*0.02* (findMotherFromContract((Contract)obj).NumOfKids);//rate per hour *hours per week * 4
+                            * (findMotherFromContract((Contract)obj).HoursNeeded) * 4)*0.02* (findMotherFromContract((Contract)obj).NumOfKids);//rate per hour *hours per week * 4 *0.02 *num of kids
                     else
                         ((Contract)obj).WagesPerHour = ((MyDal.getNannyDS().Find(x => x.Id == (((Contract)obj).NannysID)).MonthlyRate)
-                            *0.02 * (findMotherFromContract((Contract)obj).NumOfKids));//rate per hour *hours per week * 4
+                            *0.02 * (findMotherFromContract((Contract)obj).NumOfKids));//rate per month * 0.02*num of kids
                 }
 
         }
