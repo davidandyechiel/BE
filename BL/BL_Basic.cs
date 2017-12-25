d02b7dcaa714be8afc958ad10202f19b6cfe6b97
David Salem <dsalem9191@gmail.com>
2017-12-25 02:39:50 +0200

    2:39

--- BL/BL_Basic.cs
+++ BL/BL_Basic.cs
@@ -150,7 +150,7 @@ namespace BL
             }
             if (list == null)
             {
-                listPro.Sort()
+                listPro.Sort()//*************************************************************************************
                 return listPro;
             }
             return list;
